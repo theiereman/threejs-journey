9674e1cd06addadf3c26d5b079f57b8aa800f015
theiereman <thomas.eyermann@protonmail.com>
2024-12-04 14:50:55 +0100

    09 debug ui

--- chapter-one/src/script.js
+++ chapter-one/src/script.js
@@ -1,5 +1,7 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import gsap from "gsap";
 
 /**
  * Base
@@ -16,17 +18,33 @@ const sizes = {
 // Scene
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BufferGeometry();
+const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2);
 
-const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
-const positionsAttribue = new THREE.BufferAttribute(positionsArray, 3);
-geometry.setAttribute("position", positionsAttribue);
+const debugObject = {
+  color: "#134ed8",
+  subdivisions: 2,
+  spin: () => {
+    gsap.to(mesh.rotation, { duration: 1, y: mesh.rotation.y + Math.PI * 2 });
+  },
+  scale: () => {
+    gsap.to(camera.position, {
+      duration: 1,
+      z: camera.position.z * 1.75,
+    });
+    gsap.to(mesh.scale, {
+      duration: 1,
+      x: mesh.scale.x * 2,
+      ease: "bounce.out",
+    });
+  },
+};
+
+const material = new THREE.MeshBasicMaterial({
+  color: debugObject.color,
+});
 
 // Object
-const mesh = new THREE.Mesh(
-  new THREE.SphereGeometry(1),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
-);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add(mesh);
 
 // Camera
@@ -62,12 +80,6 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 // Animate
 const clock = new THREE.Clock();
 
-// Cursor
-const cursor = {
-  x: 0,
-  y: 0,
-};
-
 window.addEventListener("resize", () => {
   sizes.width = window.innerWidth;
   sizes.height = window.innerHeight;
@@ -82,6 +94,34 @@ window.addEventListener("resize", () => {
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
+
+const gui = new GUI({ title: "Debug UI" });
+gui.add(mesh.position, "y", -1, 1, 0.001);
+gui.add(mesh, "visible");
+
+gui.add(material, "wireframe");
+gui.addColor(debugObject, "color").onChange((value) => {
+  material.color.set(value);
+});
+gui.add(debugObject, "spin");
+gui.add(debugObject, "scale");
+
+gui
+  .add(debugObject, "subdivisions")
+  .step(1)
+  .min(1)
+  .max(20)
+  .onChange(() => {
+    mesh.geometry.dispose();
+    mesh.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      debugObject.subdivisions,
+      debugObject.subdivisions,
+      debugObject.subdivisions
+    );
+  });
 
 const tick = () => {
   const elapsedTime = clock.getElapsedTime();
