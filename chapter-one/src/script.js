22d084cba5da8d94dc45cffea4c628445fba4380
theiereman <thomas.eyermann@protonmail.com>
2024-12-04 09:00:03 +0100

    chapter 06

--- chapter-one/src/script.js
+++ chapter-one/src/script.js
@@ -1,48 +1,56 @@
-import * as THREE from "three";
-import gsap from "gsap";
-import "./style.css";
+import * as THREE from 'three'
 
+/**
+ * Base
+ */
 // Canvas
-const canvas = document.querySelector("canvas.webgl");
-
-// Scene
-const scene = new THREE.Scene();
-
-// Object
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const canvas = document.querySelector('canvas.webgl')
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
-};
+    width: 800,
+    height: 600
+}
+
+// Scene
+const scene = new THREE.Scene()
+
+// Object
+const mesh = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+    new THREE.MeshBasicMaterial({ color: 0xff0000 })
+)
+scene.add(mesh)
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
-scene.add(camera);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.x = 2
+camera.position.y = 2
+camera.position.z = 2
+camera.lookAt(mesh.position)
+scene.add(camera)
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas,
-});
-renderer.setSize(sizes.width, sizes.height);
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
 
-//animation
-const clock = new THREE.Clock();
+// Animate
+const clock = new THREE.Clock()
 
-gsap.to(mesh.position, { duration: 1, delay: 1, x: 2 });
-gsap.to(mesh.position, { duration: 1, delay: 2, x: 0 });
+const tick = () =>
+{
+    const elapsedTime = clock.getElapsedTime()
 
-const tick = () => {
-  const elapsedTime = clock.getElapsedTime();
+    // Update objects
+    mesh.rotation.y = elapsedTime;
 
-  //render
-  renderer.render(scene, camera);
+    // Render
+    renderer.render(scene, camera)
 
-  window.requestAnimationFrame(tick);
-};
-tick();
+    // Call tick again on the next frame
+    window.requestAnimationFrame(tick)
+}
+
+tick()
