328f9ecb3ff992bf94b7bd366043b25ede78ddcb
theiereman <thomas.eyermann@protonmail.com>
2024-12-04 11:32:11 +0100

    07 and 08 fullscreen and geometries

--- chapter-one/src/script.js
+++ chapter-one/src/script.js
@@ -9,17 +9,23 @@ const canvas = document.querySelector("canvas.webgl");
 
 // Sizes
 const sizes = {
-  width: 800,
-  height: 600,
+  width: window.innerWidth,
+  height: window.innerHeight,
 };
 
 // Scene
 const scene = new THREE.Scene();
 
+const geometry = new THREE.BufferGeometry();
+
+const positionsArray = new Float32Array([0, 0, 0, 0, 1, 0, 1, 0, 0]);
+const positionsAttribue = new THREE.BufferAttribute(positionsArray, 3);
+geometry.setAttribute("position", positionsAttribue);
+
 // Object
 const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+  new THREE.SphereGeometry(1),
+  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
 );
 scene.add(mesh);
 
@@ -51,6 +57,7 @@ const renderer = new THREE.WebGLRenderer({
   canvas: canvas,
 });
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 // Animate
 const clock = new THREE.Clock();
@@ -61,10 +68,16 @@ const cursor = {
   y: 0,
 };
 
-window.addEventListener("mousemove", (event) => {
-  cursor.x = event.clientX / sizes.width - 0.5;
-  cursor.y = -(event.clientY / sizes.height - 0.5);
-  console.log(cursor);
+window.addEventListener("resize", () => {
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(sizes.width, sizes.height);
+
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
 const controls = new OrbitControls(camera, canvas);
