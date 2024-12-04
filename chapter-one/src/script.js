9bc43e450dbf3a923140d7408bc2f459e1614f5a
theiereman <thomas.eyermann@protonmail.com>
2024-12-04 09:50:43 +0100

    06 cameras

--- chapter-one/src/script.js
+++ chapter-one/src/script.js
@@ -1,56 +1,94 @@
-import * as THREE from 'three'
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 /**
  * Base
  */
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl");
 
 // Sizes
 const sizes = {
-    width: 800,
-    height: 600
-}
+  width: 800,
+  height: 600,
+};
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 
 // Object
 const mesh = new THREE.Mesh(
-    new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
-    new THREE.MeshBasicMaterial({ color: 0xff0000 })
-)
-scene.add(mesh)
+  new THREE.BoxGeometry(1, 1, 1, 5, 5, 5),
+  new THREE.MeshBasicMaterial({ color: 0xff0000 })
+);
+scene.add(mesh);
 
 // Camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.x = 2
-camera.position.y = 2
-camera.position.z = 2
-camera.lookAt(mesh.position)
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  1,
+  100
+);
+// const aspectRatio = sizes.width / sizes.height;
+// const camera = new THREE.OrthographicCamera(
+//   -2 * aspectRatio,
+//   2 * aspectRatio,
+//   2,
+//   -2,
+//   0.1,
+//   100
+// );
+
+// camera.position.x = 2;
+// camera.position.y = 2;
+camera.position.z = 2;
+camera.lookAt(mesh.position);
+scene.add(camera);
 
 // Renderer
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
 
 // Animate
-const clock = new THREE.Clock()
+const clock = new THREE.Clock();
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+// Cursor
+const cursor = {
+  x: 0,
+  y: 0,
+};
 
-    // Update objects
-    mesh.rotation.y = elapsedTime;
+window.addEventListener("mousemove", (event) => {
+  cursor.x = event.clientX / sizes.width - 0.5;
+  cursor.y = -(event.clientY / sizes.height - 0.5);
+  console.log(cursor);
+});
 
-    // Render
-    renderer.render(scene, camera)
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
 
-tick()
+  // Update objects
+  //   mesh.rotation.y = elapsedTime;
+
+  //camera position
+  //   camera.position.x = Math.sin(cursor.x * (Math.PI * 2)) * 2;
+  //   camera.position.y = cursor.y * 2;
+  //   camera.position.z = Math.cos(cursor.x * (Math.PI * 2)) * 2;
+  //   camera.lookAt(mesh.position);
+
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
